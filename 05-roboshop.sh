02ed1f90144d16367de522578b79ff21e9e6a13c
lkamboj <kamboj.lakhnder@gmail.com>
2020-06-19 20:12:11 -0400

    Input

--- 05-roboshop.sh
+++ 05-roboshop.sh
@@ -5,9 +5,9 @@ Y="\e[1;33m"
 N="\e[0m"
 R="\e[1;31m"
 
-echo -e "[${B}INF${N}] MESSAGE"
-echo -e "[${Y}WAR${N}] WARNING"
-echo -e "[${R}ERR${N}] ERROR"
+#echo -e "[${B}INF${N}] MESSAGE"
+#echo -e "[${Y}WAR${N}] WARNING"
+#echo -e "[${R}ERR${N}] ERROR"
 
-
+echo -e "${B}[INF]${N}] SETUP MONGODB"
 
