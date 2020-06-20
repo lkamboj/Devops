96273a2625130fed0583aa2677a0175f80b77f58
lkamboj <kamboj.lakhnder@gmail.com>
2020-06-19 20:55:18 -0400

    Conditions

---
+++ 07-Conditions.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+#condition are 2 commands
+# Case
+# if
+
+##case command
+<<commants
+case variable in
+  pattern1)
+    commands
+    ;;
+    pattern2)
+    commands
+    ;;
+esac
+commants
+
+case fruit_name in
+       apple)
+         echo Fruit name is Apple
+         ;;
+       banana)
+         echo Fruit name is Banana
+         ;;
+
+esac
