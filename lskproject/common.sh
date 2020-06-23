80d19a477f3e3d07781d956e6c1da1a86c8a7e8f
lkamboj <kamboj.lakhnder@gmail.com>
2020-06-22 20:10:36 -0400

    Conditions

--- lskproject/common.sh
+++ lskproject/common.sh
@@ -18,6 +18,13 @@ PRINT() {
   echo -e "[${B}INF${N}] $1"
 }
 
+echo '[mongodb-org-4.2]
+name=MongoDB Repository
+baseurl=https://repo.mongodb.org/yum/redhat/$releasever/mongodb-org/4.2/x86_64/
+gpgcheck=1
+enabled=1
+gpgkey=https://www.mongodb.org/static/pgp/server-4.2.asc' >/etc/yum.repos.d/mongodb.repo
+
 ERROR() {
   echo -e "[${R}ERROR${N}] $1"
 }
