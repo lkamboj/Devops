ff15f242acb4adc4a8f3c0ebe1297ed4c8e9ce5c
lkamboj <kamboj.lakhnder@gmail.com>
2020-06-22 20:11:03 -0400

    Conditions

--- lskproject/MONGODB.sh
+++ lskproject/MONGODB.sh
@@ -4,5 +4,12 @@
 
 source $(dirname $0)/common.sh
 
+echo '[mongodb-org-4.2]
+name=MongoDB Repository
+baseurl=https://repo.mongodb.org/yum/redhat/$releasever/mongodb-org/4.2/x86_64/
+gpgcheck=1
+enabled=1
+gpgkey=https://www.mongodb.org/static/pgp/server-4.2.asc' >/etc/yum.repos.d/mongodb.repo
+
 PRINT "START MONGODB SETUP"
 PRINT "END MONGODB SETUP"
