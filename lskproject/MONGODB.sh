2e3e9fccec89860ffc689ae508eefc8d81d10489
lkamboj <kamboj.lakhnder@gmail.com>
2020-06-22 20:38:06 -0400

    Conditions

--- lskproject/MONGODB.sh
+++ lskproject/MONGODB.sh
@@ -7,6 +7,8 @@ source $(dirname $0)/common.sh
 PRINT "START MONGODB SETUP"
 
 echo LOG = ${LOG_FILE}
+#The above line is to test if we have permissions to the log file
+
 echo '[mongodb-org-4.2]
 name=MongoDB Repository
 baseurl=https://repo.mongodb.org/yum/redhat/$releasever/mongodb-org/4.2/x86_64/
